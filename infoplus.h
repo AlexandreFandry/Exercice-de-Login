b900cf56b62a4d8b1f2f58d97b0550f7590fb7fe
AlexandreFandry <167619976+AlexandreFandry@users.noreply.github.com>
2024-09-05 13:38:51 +0300

    Add files via upload

---
+++ infoplus.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+void start();
+void end();
+void IPuser(int bit1, int bit2, int bit3, int bit4, int bits);
+void calcul(int bit1, int bit2, int bit3, int bit4, int bits);
+void calcul(int bit1, int bit2, int bit3, int bit4, int bits);
